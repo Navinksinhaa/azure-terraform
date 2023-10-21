f830a4ee9ec97367e3d2ed2311a7214ed9ba6d44
Navin Kumar <nksinha9993@gmail.com>
2023-10-21 11:54:54 +0530

    terraform provision

---
+++ main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=3.0.1"
+    }
+  }
+}
+
+# 2. Configure the AzureRM Provider
+provider "azurerm" {
+  # The AzureRM Provider supports authenticating using via the Azure CLI, a Managed Identity
+  # and a Service Principal. More information on the authentication methods supported by
+  # the AzureRM Provider can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs#authenticating-to-azure
+
+  # The features block allows changing the behaviour of the Azure Provider, more
+  # information can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/features-block
+  features {}
+}
+
+# 3. Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+# 4. Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+
+
+
+resource "azurerm_public_ip" "example" {
+  name                = "exampip"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_subnet" "example1" {
+  name                 = "backend"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+  
+}
+
+resource "azurerm_subnet" "example2" {
+  name                 = "frontend"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.0.0/24"]
+  
+}
+
+data "azurerm_subnet" "example1" {
+  name                 = "backend"
+  virtual_network_name = "example-network"
+  resource_group_name  = azurerm_resource_group.example.name
+}
+
+output "subnet_id" {
+  value = data.azurerm_subnet.example1.id
+}
+
+
